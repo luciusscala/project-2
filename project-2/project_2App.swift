67e38522fd69c8d05f7bb5d603aaf643b75eec7a
Lucius <luciustheperson@gmail.com>
2026-04-01 13:43:37 -0700

    Initial Commit

---
+++ project-2/project_2App.swift
@@ -0,0 +1,17 @@
+//
+//  project_2App.swift
+//  project-2
+//
+//  Created by Lucius Scala on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct project_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ project-2Tests/project_2Tests.swift
@@ -0,0 +1,19 @@
+//
+//  project_2Tests.swift
+//  project-2Tests
+//
+//  Created by Lucius Scala on 4/1/26.
+//
+
+import Testing
+@testable import project_2
+
+struct project_2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
